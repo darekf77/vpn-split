550e9c26a0383b8a1c7e8ccc65d6e17400f5df84
Dariusz Filipiak <darekf77@gmail.com>
2025-10-02 22:54:26 +0200

    chore: update

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,37 @@
-//#region @notForNpm
-import { HOST_BACKEND_PORT } from './app.hosts';
-//#region @browser
-import { NgModule } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+//#region imports
+import * as os from 'os'; // @backend
 
+import { CommonModule } from '@angular/common'; // @browser
+import { NgModule, inject, Injectable } from '@angular/core'; // @browser
+import { Component, OnInit } from '@angular/core'; // @browser
+import { VERSION } from '@angular/core'; // @browser
+import Aura from '@primeng/themes/aura'; // @browser
+import { MaterialCssVarsModule } from 'angular-material-css-vars'; // @browser
+import { providePrimeNG } from 'primeng/config'; // @browser
+import { Observable, map } from 'rxjs';
+import { Taon, BaseContext, TAON_CONTEXT } from 'taon/src';
+import { UtilsOs } from 'tnp-core/src';
+
+import { HOST_CONFIG } from './app.hosts';
+//#endregion
+
+console.log('hello world');
+console.log('Your backend host ' + HOST_CONFIG['MainContext'].host);
+console.log('Your frontend host ' + HOST_CONFIG['MainContext'].frontendHost);
+
+//#region vpn-split component
+//#region @browser
 @Component({
   selector: 'app-vpn-split',
-  template: 'hello from vpn-split',
+  standalone: false,
+  template: `hello from vpn-split<br />
+    Angular version: {{ angularVersion }}<br />
+    <br />
+    users from backend
+    <ul>
+      <li *ngFor="let user of users$ | async">{{ user | json }}</li>
+    </ul>
+    hello world from backend: <strong>{{ hello$ | async }}</strong> `,
   styles: [
     `
       body {
@@ -15,26 +40,170 @@ import { Component, OnInit } from '@angular/core';
     `,
   ],
 })
-export class VpnSplitComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class VpnSplitComponent {
+  angularVersion =
+    VERSION.full +
+    ` mode: ${UtilsOs.isRunningInWebSQL() ? ' (websql)' : '(normal)'}`;
+  userApiService = inject(UserApiService);
+  readonly users$: Observable<User[]> = this.userApiService.getAll();
+  readonly hello$ = this.userApiService.userController
+    .helloWorld()
+    .request()
+    .observable.pipe(map(r => r.body.text));
 }
+//#endregion
+//#endregion
 
+//#region  vpn-split api service
+//#region @browser
+@Injectable({
+  providedIn: 'root',
+})
+export class UserApiService extends Taon.Base.AngularService {
+  userController = this.injectController(UserController);
+  getAll(): Observable<User[]> {
+    return this.userController
+      .getAll()
+      .request()
+      .observable.pipe(map(r => r.body.json));
+  }
+}
+//#endregion
+//#endregion
+
+//#region  vpn-split module
+//#region @browser
 @NgModule({
-  imports: [],
+  providers: [
+    {
+      provide: TAON_CONTEXT,
+      useFactory: () => MainContext,
+    },
+    providePrimeNG({
+      // inited ng prime - remove if not needed
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
   exports: [VpnSplitComponent],
+  imports: [
+    CommonModule,
+    MaterialCssVarsModule.forRoot({
+      // inited angular material - remove if not needed
+      primary: '#4758b8',
+      accent: '#fedfdd',
+    }),
+  ],
   declarations: [VpnSplitComponent],
-  providers: [],
 })
 export class VpnSplitModule {}
 //#endregion
+//#endregion
 
-async function start() {
-  console.log('hello world');
-  console.log('Please start your server on port: ' + HOST_BACKEND_PORT);
+//#region  vpn-split entity
+@Taon.Entity({ className: 'User' })
+class User extends Taon.Base.AbstractEntity {
+  //#region @websql
+  @Taon.Orm.Column.String()
+  //#endregion
+  name?: string;
+}
+//#endregion
+
+//#region  vpn-split controller
+@Taon.Controller({ className: 'UserController' })
+class UserController extends Taon.Base.CrudController<User> {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  entityClassResolveFn = () => User;
+
+  @Taon.Http.GET()
+  helloWorld(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => 'hello world';
+    //#endregion
+  }
+
+  @Taon.Http.GET()
+  getOsPlatform(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => {
+      //#region @backend
+      return os.platform(); // for normal nodejs backend return real value
+      //#endregion
+      return 'no-platform-inside-browser-and-websql-mode';
+    };
+    //#endregion
+  }
+}
+//#endregion
+
+//#region  vpn-split migration
+//#region @websql
+@Taon.Migration({
+  className: 'UserMigration',
+})
+class UserMigration extends Taon.Base.Migration {
+  userController = this.injectRepo(User);
+  async up(): Promise<any> {
+    const superAdmin = new User();
+    superAdmin.name = 'super-admin';
+    await this.userController.save(superAdmin);
+  }
+}
+//#endregion
+//#endregion
+
+//#region  vpn-split context
+var MainContext = Taon.createContext(() => ({
+  ...HOST_CONFIG['MainContext'],
+  contexts: { BaseContext },
+  //#region @websql
+  /**
+   * This is dummy migration - you DO NOT NEED need this migrations object
+   * if you are using HOST_CONFIG['MainContext'] that contains 'migrations' object.
+   * DELETE THIS 'migrations' object if you use taon CLI that generates
+   * migrations automatically inside /src/migrations folder.
+   */
+  migrations: {
+    UserMigration,
+  },
+  //#endregion
+  controllers: {
+    UserController,
+  },
+  entities: {
+    User,
+  },
+  database: true,
+  // disabledRealtime: true,
+}));
+//#endregion
+
+async function start(startParams?: Taon.StartParams): Promise<void> {
+  await MainContext.initialize();
+
+  //#region @backend
+  if (
+    startParams.onlyMigrationRun ||
+    startParams.onlyMigrationRevertToTimestamp
+  ) {
+    process.exit(0);
+  }
+  //#endregion
+
+  //#region @backend
+  console.log(`Hello in NodeJs backend! os=${os.platform()}`);
+  //#endregion
+
+  if (UtilsOs.isBrowser) {
+    const users = (
+      await MainContext.getClassInstance(UserController).getAll().request()
+    ).body?.json;
+    console.log({
+      'users from backend': users,
+    });
+  }
 }
 
 export default start;
-
-//#endregion
